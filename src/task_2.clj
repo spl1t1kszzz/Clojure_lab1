44a0d7883db26f4de33743035ad4fa27d9ef2989
Nikita <nikita17403@yandex.ru>
2025-11-12 16:43:13 +0700

    2.1 + 2.2

--- src/task_2.clj
+++ src/task_2.clj
@@ -4,17 +4,31 @@
 
 ; left = (f(0) + f(x)) / 2
 ; right = sum (f(x_i))
-(defn integrate [f x h]
-  (let [left (/ (+ (f 0) (f x)) 2) right (reduce (fn [acc x] (+ acc (f x))) 0 (range 0 (+ x h) h))]
-    (* h (+ left right)))
-)
+(defn integrate [f h]
+  (fn [x] (let [left (/ (+ (f 0) (f x)) 2) right (reduce (fn [acc t] (+ acc (f t))) 0 (range 0 (+ x h) h))]
+            (* h (+ left right)))))
+; 2.1
+(defn m_integrate [f h]
+  (memoize (integrate f h)))
 
-(def m_integrate (memoize (fn [f x h] (integrate f x h))))
+; 2.2
+(defn integrate-seq [f h]
+  (map second (iterate (fn [[x acc]] [(+ x h) (+ acc (* h 0.5 (+ (f x) (f (+ x h)))))]) [0 0])))
+
+(defn make-integral [f h]
+  (let [seqF (integrate-seq f h)]
+    (fn [x] (nth seqF (int (/ x h))))))
 
 
 (defn -main []
-  (let [h 0.00000001]
-    (println (time (integrate square 3 h)))
-    (println (time (m_integrate square 3 h)))
-    (println (time (m_integrate square 3 h)))
+  (let [h 0.00001 F (m_integrate square h) F1 (make-integral square h)]
+    (time (println (F 10)))
+    (time (println (F 10)))
+    (time (println "F(10):" (F1 10)))
+    (time (println "F(100):" (F1 100)))
+    (time (println "F(90):" (F1 90)))
+    (time (println "F(200):" (F1 200)))
+    (time (println "F(150):" (F1 150)))
+    (time (println "F(199):" (F1 199)))
+    (time (println "F(200):" (F1 200)))
     ))
