98b0c3ccfeba983b8e7e554e150c6402024a5ee3
Nikita <nikita17403@yandex.ru>
2025-11-28 19:40:31 +0700

    3.1 refactor + 3.2

--- src/task_3.clj
+++ src/task_3.clj
@@ -1,27 +1,29 @@
 (ns task-3)
 
-(defn parallel-filter-optimal
-  [pred coll block-size]
-  (let [v (vec coll)
-        n (count v)
-        starts (range 0 n block-size)
-        futures (map (fn [start]
-                       (future
-                         (let [end (min n (+ start block-size))]
-                           (loop [i start result []]
-                             (if (< i end)
-                               (let [item (v i)]
-                                 (recur (inc i)
-                                        (if (pred item)
-                                          (conj result item)
-                                          result)))
-                               result)))))
-                     starts)
-        results (map deref futures)]
-    (apply concat results)))
+(defn parallel_filter [pred coll block_size]
+  (loop [blocks coll futures []]
+    (if (empty? blocks)
+      (apply concat (map deref futures))
+      (let [block (vec (take block_size blocks)) fut (future (doall (filter pred block)))]
+        (recur (drop block_size blocks)
+               (conj futures fut))))))
+
+(defn lazy-parallel-filter [pred coll block-size]
+  (letfn [(spawn_futures [s]
+            (if-let [blocks (seq s)]
+              (cons (future (doall (filter pred (take block-size blocks)))) (spawn_futures (drop block-size blocks)))
+              ()))
+
+          (collect_results [fs]
+            (lazy-seq
+              (if-let [f (seq fs)]
+                (concat (deref (first f)) (collect_results (rest f)))
+                ())))]
+
+    (collect_results (spawn_futures coll))))
 
 
-(defn prime? [n]
+(defn is_prime [n]
   (if (< n 2)
     false
     (let [_ (Thread/sleep 1)
@@ -38,12 +40,23 @@
                            :else (recur (inc i))))))]
       is-prime)))
 
-(defn test-performance [block-size]
-  (let [data (range 1 10001) pred prime?]
-    (time (doall (filter pred data)))
-    (time (doall (parallel-filter-optimal pred data block-size)))))
+(defn run_test [block-size]
+  (let [data (range 1 2001)
+        pred is_prime]
+
+    (println "Обычный filter")
+    (let [res (time (doall (filter pred data)))]
+      (println (take 20 res)))
+
+    (println "Параллельный filter")
+    (let [res (time (parallel_filter pred data block-size))]
+      (println (take 20 res)))
+
+    (println "Ленивый параллельный filter")
+    (let [res (time (doall (lazy-parallel-filter pred data block-size)))]
+      (println (take 20 res)))))
 
 
 (defn -main []
-  (test-performance 2000)
+  (run_test 400)
 )
